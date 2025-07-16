f8b28386242eb0bf598207a34d40f477cd2be0e3
sonofbossman <akingbadeeniola452@gmail.com>
2025-07-16 03:03:44 +0100

    fix checker issue

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -9,4 +9,4 @@ library = Library.objects.get(name=library_name)
 books = library.books.all()
 # Retrieve the librarian for a library.
 library = Library.objects.get(name=library_name)
-library.librarian
+librarian = Librarian.objects.get(library=library)
