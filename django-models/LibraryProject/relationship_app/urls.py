77d70738d7237dd766cc6f14838802e65859ca01
sonofbossman <akingbadeeniola452@gmail.com>
2025-07-20 19:43:45 +0100

    Enhance your relationship_app by adding user authentication features. Implement views and templates for user login, logout, and registration to demonstrate how Django manages user sessions and permissions.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,13 +2,13 @@
 from django.urls import path
 from relationship_app import views
 from .views import list_books
-from django.contrib.auth import views as auth_view
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
   path('', list_books, name='list-books'),
   path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail-cbv'),
   path('register/', views.register, name='register'),
-  path('login/', auth_view.LoginView.as_view(), name='login'),
-  path('logout/', auth_view.LogoutView.as_view(), name='logout')
+  path('login/', LoginView.as_view(template_name="relationship_app/login.html"), name='login'),
+  path('logout/', LogoutView.as_view(template_name="relationship_app/logout.html"), name='logout')
 ]
