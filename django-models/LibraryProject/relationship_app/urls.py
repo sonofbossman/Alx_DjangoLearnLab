617080fae9c1c262f1614e5658fc1cfa43cc28a4
sonofbossman <akingbadeeniola452@gmail.com>
2025-07-20 19:38:09 +0100

    Enhance your relationship_app by adding user authentication features. Implement views and templates for user login, logout, and registration to demonstrate how Django manages user sessions and permissions.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,14 +1,14 @@
 
 from django.urls import path
 from relationship_app import views
-from .views import list_books, RegisterView
+from .views import list_books
 from django.contrib.auth import views as auth_view
 
 
 urlpatterns = [
   path('', list_books, name='list-books'),
   path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail-cbv'),
-  path('register/', RegisterView.as_view(), name='register'),
+  path('register/', views.register, name='register'),
   path('login/', auth_view.LoginView.as_view(), name='login'),
   path('logout/', auth_view.LogoutView.as_view(), name='logout')
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from .models import Library, Author, Book, Librarian
 from django.views.generic.edit import FormView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.contrib.auth import login
 
 # Create your views here.
 def list_books(request):
@@ -23,12 +24,23 @@ class LibraryDetailView(DetailView):
     context = super().get_context_data(**kwargs)
     context['books'] = self.object.books.all()
     return context
-  
-class RegisterView(FormView):
-  template_name = 'registration/register.html'
-  form_class = UserCreationForm
-  success_url = reverse_lazy('login')
 
-  def form_valid(self, form):
-    form.save()
-    return super().form_valid(form)
+def register(request):
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('list-books')
+  else:
+    form = UserCreationForm()
+  return render(request, 'registration/register.html', {'form':form})  
+
+# class RegisterView(FormView):
+#   template_name = 'registration/register.html'
+#   form_class = UserCreationForm
+#   success_url = reverse_lazy('login')
+
+#   def form_valid(self, form):
+#     form.save()
+#     return super().form_valid(form)
