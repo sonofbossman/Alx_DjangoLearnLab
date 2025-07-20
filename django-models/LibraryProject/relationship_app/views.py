42bce4d07c9892456a77db30ceabeb51d6c864dd
sonofbossman <akingbadeeniola452@gmail.com>
2025-07-20 19:26:47 +0100

    Enhance your relationship_app by adding user authentication features. Implement views and templates for user login, logout, and registration to demonstrate how Django manages user sessions and permissions.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Library, Author, Book, Librarian
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 def list_books(request):
@@ -20,3 +23,12 @@ class LibraryDetailView(DetailView):
     context = super().get_context_data(**kwargs)
     context['books'] = self.object.books.all()
     return context
+  
+class RegisterView(FormView):
+  template_name = 'registration/register.html'
+  form_class = UserCreationForm
+  success_url = reverse_lazy('login')
+
+  def form_valid(self, form):
+    form.save()
+    return super().form_valid(form)
