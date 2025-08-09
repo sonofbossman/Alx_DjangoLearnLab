dea8ad7c0c53ca37c523d54127bfb0bebb198284
sonofbossman <akingbadeeniola452@gmail.com>
2025-08-09 22:16:40 +0100

    Apply Django REST Framework’s permission classes to protect API endpoints based on user roles.

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,24 +2,30 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Author, Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 # Create your views here.
 
 class ListView(generics.ListAPIView):
   queryset = Book.objects.all()
   serializer_class = BookSerializer
+  permission_classes = [IsAuthenticatedOrReadOnly]
 
 class DetailView(generics.RetrieveAPIView):
   queryset = Book.objects.all()
   serializer_class = BookSerializer
+  permission_classes = [IsAuthenticatedOrReadOnly]
 
 class CreateView(generics.CreateAPIView):
   queryset = Book.objects.all()
   serializer_class = BookSerializer
+  permission_classes = [IsAuthenticated]
 
 class UpdateView(generics.UpdateAPIView):
   queryset = Book.objects.all()
   serializer_class = BookSerializer
+  permission_classes = [IsAuthenticated]
 
 class DeleteView(generics.DestroyAPIView):
   queryset = Book.objects.all()
   serializer_class = BookSerializer
+  permission_classes = [IsAuthenticated]
