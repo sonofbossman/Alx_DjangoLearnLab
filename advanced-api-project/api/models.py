216ba1cba4761e56414ab5369de6894c9f0ebb78
sonofbossman <akingbadeeniola452@gmail.com>
2025-08-09 20:47:40 +0100

    Define Data Models, Create Custom Serializers, and Document Model and Serializer Setup

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+  """
+  Defines the object representation of an author.
+  """
+  
+  name = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
+
+class Book(models.Model):
+  """
+  Defines the object representation of a book with its author.
+  """
+
+  title = models.CharField(max_length=100)
+  publication_year = models.PositiveIntegerField()
+  author = models.ForeignKey(Author, related_name="books", on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.title
---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import Book, Author
+from datetime import datetime
+from rest_framework.validators import ValidationError
+
+class BookSerializer(serializers.ModelSerializer):
+  """
+  Serializes all fields of the Book model.
+  Includes a custom validation to the BookSerializer to 
+  ensure the publication_year is not in the future.
+  """
+
+  class Meta:
+    model = Book
+    fields = "__all__"
+  
+  def validate_publication_year(self, value):
+    if value > datetime.now().year:
+      raise ValidationError("publication_year cannot be in the future!")
+    pass
+
+class AuthorSerializer(serializers.ModelSerializer):
+  """
+  Serializes all fields of the Author model, 
+  and nested BookSerializer to serialize the related books dynamically.
+  """
+  
+  books = BookSerializer(many=True, read_only=True)
+
+  class Meta:
+    model = Author
+    fields = "__all__"
