178a0bbdec0a481eefbbefdf35d6162e9040489e
sonofbossman <akingbadeeniola452@gmail.com>
2025-08-09 20:53:34 +0100

    Register models in admin.py

--- advanced-api-project/api/admin.py
+++ advanced-api-project/api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Author, Book
 
 # Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+  list_display = ['name']
+
+class BookAdmin(admin.ModelAdmin):
+  list_display = ['title', 'publication_year', 'author']
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
