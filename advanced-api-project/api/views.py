5171a459486e2b2842bdd815980ea00f57f4c327
sonofbossman <akingbadeeniola452@gmail.com>
2025-08-10 13:53:59 +0100

    Implement Filtering, Searching, and Ordering in Django REST Framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,41 +2,65 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Author, Book
 from .serializers import BookSerializer
+from django.db.models import Q
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 class BookListView(ListView):
   model = Book
-  fields = "__all__"
-  template_name = "book_list.html"
-  # serializer_class = BookSerializer
-  permission_classes = [IsAuthenticatedOrReadOnly]
+  template_name = 'book_list.html'
+  context_object_name = 'books'
+
+  def get_queryset(self):
+    queryset = super().get_queryset()
+
+    # Filtering
+    author = self.request.GET.get('author')
+    publication_year = self.request.GET.get('publication_year')
+    if author:
+        queryset = queryset.filter(author__iexact=author)
+    if publication_year:
+        queryset = queryset.filter(publication_year=publication_year)
+
+    # Searching
+    search_term = self.request.GET.get('search')
+    if search_term:
+        queryset = queryset.filter(
+            Q(title__icontains=search_term) |
+            Q(author__icontains=search_term)
+        )
+
+    # Ordering
+    ordering = self.request.GET.get('ordering')
+    if ordering:
+        queryset = queryset.order_by(ordering)
+
+    return queryset
 
 class BookDetailView(DetailView):
   model = Book
   template_name = "book_detail.html"
+  context_object_name = 'book'
 
   # serializer_class = BookSerializer
-  permission_classes = [IsAuthenticatedOrReadOnly]
 
-class BookCreateView(CreateView):
+class BookCreateView(LoginRequiredMixin, CreateView):
   model = Book
   fields = "__all__"
   template_name = "book_form.html"
   success_url = "/books/"
   # serializer_class = BookSerializer
-  permission_classes = [IsAuthenticated]
 
-class BookUpdateView(UpdateView):
+class BookUpdateView(LoginRequiredMixin, UpdateView):
   model = Book
   fields = "__all__"
   template_name = "book_form.html"
   # serializer_class = BookSerializer
-  permission_classes = [IsAuthenticated]
+  success_url = "/books/"
 
-class BookDeleteView(DeleteView):
+class BookDeleteView(LoginRequiredMixin, DeleteView):
   model = Book
   success_url = "/books/"
   template_name = "book_confirm_delete.html"
   # serializer_class = BookSerializer
-  permission_classes = [IsAuthenticated]
